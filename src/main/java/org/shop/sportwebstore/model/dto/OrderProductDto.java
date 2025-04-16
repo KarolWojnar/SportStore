f6ed15252e61015b38cb72713928e89bfd48ade8
KarolWojnar <103566194+KarolWojnar@users.noreply.github.com>
2025-04-16 10:14:40 +0200

    Update OrderProductDto.java

--- src/main/java/org/shop/sportwebstore/model/dto/OrderProductDto.java
+++ src/main/java/org/shop/sportwebstore/model/dto/OrderProductDto.java
@@ -13,4 +13,20 @@ public class OrderProductDto {
     private String name;
     private String image;
     private boolean isRated;
+
+    public static List<OrderProductDto> mapToDto(List<ProductInOrder> products, List<Product> productList) {
+        return products.stream().map(product -> {
+            Product product1 = productList.stream()
+                    .filter(p -> p.getId().equals(product.getProductId()))
+                    .findFirst().orElseThrow(() -> new ProductException("Product not found"));
+            return OrderProductDto.builder()
+                    .productId(product.getProductId())
+                    .quantity(product.getAmount())
+                    .price(product.getPrice())
+                    .name(product1.getName())
+                    .image(product1.getImageUrl())
+                    .isRated(product.isRated())
+                    .build();
+        }).toList();
+    }
 }
